50b70011782e2b3937332d5910d9232e59f266bc
thyagopacher <thyago.pacher@gmail.com>
2021-02-28 18:22:32 -0300

    primeiro

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../Users/ThyagoHenrique/Google Drive/Programação/PHP/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
